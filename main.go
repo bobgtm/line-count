53b84fe5396f1af7b237aafc5dd338019e85edc6
Bobby Griffin <robertgriffintm@gmail.com>
2023-12-04 12:39:51 -0500

    add: line count total for targeted directory

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 	files := WalkDirectory(".")
 
 	ignoreRequest := []string{"main.go", "gitignore"}
-
+	total := 0
 	for k, v := range files {
 		for i := 0; i < len(ignoreRequest); i++ {
 			if strings.Contains(v.name, ignoreRequest[i]) {
@@ -21,5 +21,7 @@ func main() {
 		if v.lines != 0 {
 			fmt.Println(v.name, v.lines)
 		}
+		total += v.lines
 	}
+	fmt.Println("Total lines in codebase:", total)
 }
